f2fb8f490da9b02f5430ca78765307d8431829b8
Ankit Sarraf <sarrafan@buffalo.edu>
2016-05-05 00:07:48 -0700

    Update create_tomcat_home.rb

--- cookbook/tomcat/recipes/create_tomcat_home.rb
+++ cookbook/tomcat/recipes/create_tomcat_home.rb
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Recipe to create tomcat home directory
 directory "#{node["tomcat"]["home"]["directory"]}" do
 	owner "root"
 	group "root"
